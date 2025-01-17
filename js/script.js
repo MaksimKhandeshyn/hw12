635a4e6b19222db0a1883bf6b6f0a90732c58dfe
MaksimKhandeshyn <145330399+MaksimKhandeshyn@users.noreply.github.com>
2025-01-18 00:32:04 +0200

    almost do hw

--- js/script.js
+++ js/script.js
@@ -41,3 +41,60 @@ const findBestEmployee = (employees) => {
   return Math.max(...kyes);
 };
 findBestEmployee();
+//--4--
+const countTotalSalary = (employees) => {
+  employees = {
+    junior: 1000,
+    middle: 3000,
+    senior: 5000,
+  };
+  const arr = Object.values(employees);
+  let res;
+  for (let i in arr) {
+    res = arr[i] + arr[i];
+  }
+  const keys = Object.entries(employees);
+  for (const i of keys) {
+    const pos = i[0];
+    const sal = i[1];
+    console.log(`${pos}: ${sal}`);
+  }
+  console.log(res);
+  return res;
+};
+countTotalSalary();
+//--5--
+const getAllPropValue = (arr, prop) => {
+  arr = [
+    { name: "firstObj", value: 0 },
+    { name: "secondObj", value: 1 },
+    { name: "thirdObj", value: 2 },
+  ];
+  console.log(arr);
+  const name = Object.entries(...arr);
+  for (let i of name) {
+    const prop = i[0];
+    console.log(`This is value: ${prop}.`);
+  }
+  return prop;
+};
+getAllPropValue();
+//--6--
+const calculateTotalPrice = (allProducts, productName) => {
+  allProducts = [
+    { name: "meat", cost: 241, amount: 2 },
+    { name: "CocaCola", cost: 36, amount: 10 },
+    { name: "apple", cost: 10, amount: 5 },
+  ];
+  const arrName = Object.values(...allProducts);
+  for (let i of arrName) {
+    const fe = i[0];
+    console.log(fe);
+  }
+  // for (let i of arrName) {
+  //   productName = i[0];
+  //   console.log(productName);
+  // }
+  console.log(arrName);
+};
+calculateTotalPrice();
